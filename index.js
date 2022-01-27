30e591c3acef085ce76995f5debb7fdbed51bf9a
esperanzaWG <myfrick57@hotmail.com>
2022-01-27 10:40:42 -0500

    all app on nodejs and express

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+
+// configure CORS
+app.use( cors() );
+
+// routes
+app.use('/', require('./rebate.js'));
+
+ 
+  
+const port = 3000;
+
+
+app.listen(port, () => {
+    console.log("App is running on port " + port);
+});
---
+++ rebate.js
@@ -0,0 +1,61 @@
+const { Router} = require('express');
+const router = Router();
+
+const axios = require('axios');
+
+
+const service='http://devapi.coolcalc.com/search-equipment';
+
+//basic authentication 
+const verify = function(){
+    let ClientId = 'DCNE';
+    let APIKey = 'letmein';
+    let encoded = Buffer.from(ClientId + ':' + APIKey).toString('base64');
+    return encoded;
+}
+
+// routes
+router.get('*', async(req, res) => {
+
+    const myUrl = req.originalUrl;
+    const newParams = myUrl.replace('/?params=', '?params=' );
+    
+    
+     /* //send the headers
+     res.setHeader('Content-Type', response.headers['content-type']);
+     res.setHeader('allow', response.headers['allow']);
+     res.setHeader('Access-Control-Expose-Headers', 'Location, Allow');
+  */
+
+     try {
+        let response = await axios.get(service,  {
+            params: {
+                params: newParams
+            },
+            headers:{
+                Authorization: `Basic ${verify()}`
+            }
+        });
+        res.json({
+            ok: true,
+            msg: 'estamos en heroku',
+            body: response.data,
+            auth: `Basic ${verify()}`
+
+        });        
+    } catch (err) {
+        res.json({
+            url1: service,
+            url2: newParams,
+            error: err,
+            auth: `Basic ${verify()}`
+        }); 
+    }
+
+})
+
+
+
+
+
+module.exports = router;
